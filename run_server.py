1aebaf003a96c5421f2753ac9848b02492fe9458
J-Spade <j_spade@verizon.net>
2017-04-19 22:22:16 -0400

    markov fix

--- run_server.py
+++ run_server.py
@@ -60,7 +60,7 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         """Save the dictionary to disk"""
         MarkovReqHandler.dictLock.acquire()
         output = open('Markov_Dict.pkl', 'w')
-        pickle.dump(dictionary, output)
+        pickle.dump(MarkovReqHandler.dictionary, output)
         output.close()
         MarkovReqHandler.dictLock.release()
 
@@ -69,37 +69,37 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         """Load the dictionary file"""
         MarkovReqHandler.dictLock.acquire()
         input = open('Markov_Dict.pkl', 'r')
-        dictionary = pickle.load(input)
+        MarkovReqHandler.dictionary = pickle.load(input)
         input.close()
         MarkovReqHandler.dictLock.release()
 
     @staticmethod
     def count_dictionary():
         print 'Counting words...'            
-        for wordpair in dictionary:
+        for wordpair in MarkovReqHandler.dictionary:
 
             temp = wordpair.split()
             uses = 0
-            for temp in dictionary.get(wordpair)[0]:
+            for temp in MarkovReqHandler.dictionary.get(wordpair)[0]:
                 uses = uses + temp[1]
-            paircounts[wordpair] = uses
+            MarkovReqHandler.paircounts[wordpair] = uses
 
             tally = 0
-            for prev in dictionary.get(wordpair)[0]:
+            for prev in MarkovReqHandler.dictionary.get(wordpair)[0]:
                 tally += prev[1]
 
             first = wordpair.split()[0]
-            if not (wordcounts.has_key(first)):
-                wordcounts[first] = 0
-            wordcounts[first] = wordcounts.get(first) + tally
+            if not (MarkovReqHandler.wordcounts.has_key(first)):
+                MarkovReqHandler.wordcounts[first] = 0
+            MarkovReqHandler.wordcounts[first] = MarkovReqHandler.wordcounts.get(first) + tally
     
-            if wordpair != STOPWORD:
+            if wordpair != MarkovReqHandler.STOPWORD:
                 second = wordpair.split()[1]
-                if not (wordcounts.has_key(second)):
-                    wordcounts[second] = 0
-                wordcounts[second] = wordcounts.get(second) + tally
+                if not (MarkovReqHandler.wordcounts.has_key(second)):
+                    MarkovReqHandler.wordcounts[second] = 0
+                MarkovReqHandler.wordcounts[second] = MarkovReqHandler.wordcounts.get(second) + tally
 
-        sentences_ever = wordcounts.get(STOPWORD)
+        sentences_ever = MarkovReqHandler.wordcounts.get(MarkovReqHandler.STOPWORD)
 
     @staticmethod
     def interpret_message(message):
@@ -107,15 +107,15 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     
         MarkovReqHandler.dictLock.acquire()
         words = message.split()
-        words.append(STOPWORD)
-        words.insert(0, STOPWORD)
+        words.append(MarkovReqHandler.STOPWORD)
+        words.insert(0, MarkovReqHandler.STOPWORD)
 
         sentences_ever = sentences_ever + 1
 
         for word in words:
-            if not (wordcounts.has_key(word)):
-                wordcounts[word] = 0
-            wordcounts[word] = wordcounts.get(word) + 1
+            if not (MarkovReqHandler.wordcounts.has_key(word)):
+                MarkovReqHandler.wordcounts[word] = 0
+            MarkovReqHandler.wordcounts[word] = MarkovReqHandler.wordcounts.get(word) + 1
 
         index = 0
         word = words[index]
@@ -130,13 +130,13 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 # this means we got to the end of the sentence
                 break
 
-            if not (paircounts.has_key(wordpair)):
-                paircounts[wordpair] = 0
-            paircounts[wordpair] = paircounts.get(wordpair) + 1
+            if not (MarkovReqHandler.paircounts.has_key(wordpair)):
+                MarkovReqHandler.paircounts[wordpair] = 0
+            MarkovReqHandler.paircounts[wordpair] = MarkovReqHandler.paircounts.get(wordpair) + 1
 
             # add 'next' as a word that comes after 'wordpair'
-            if dictionary.has_key(wordpair):
-                temp = dictionary.get(wordpair)[1]
+            if MarkovReqHandler.dictionary.has_key(wordpair):
+                temp = MarkovReqHandler.dictionary.get(wordpair)[1]
                 wordindex = word_index_in_list(next, temp)
                 if wordindex == -1:
                     temp.append((next, 1))
@@ -144,11 +144,11 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                     prevcount = temp[wordindex][1]
                     temp[wordindex] = (next, prevcount + 1)
             else:
-                dictionary[wordpair] = ([], [(next, 1)])
+                MarkovReqHandler.dictionary[wordpair] = ([], [(next, 1)])
 
             # add 'word' as a word that comes before 'nextpair'
-            if dictionary.has_key(nextpair):
-                othertemp = dictionary.get(nextpair)[0]
+            if MarkovReqHandler.dictionary.has_key(nextpair):
+                othertemp = MarkovReqHandler.dictionary.get(nextpair)[0]
                 wordindex = word_index_in_list(word, othertemp)
                 if wordindex == -1:
                     othertemp.append((word, 1))
@@ -157,7 +157,7 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                     othertemp[wordindex] = (word, prevcount + 1)
 
             else:
-                dictionary[nextpair] = ([(word, 1)], [])
+                MarkovReqHandler.dictionary[nextpair] = ([(word, 1)], [])
 
             index = index + 1
             word = words[index]
@@ -172,29 +172,29 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         MarkovReqHandler.dictLock.acquire()
 
         words = message.split()
-        words.append(STOPWORD)
-        words.insert(0, STOPWORD)
+        words.append(MarkovReqHandler.STOPWORD)
+        words.insert(0, MarkovReqHandler.STOPWORD)
 
         if len(words) < 2:
             return ''
 
         # try to guess which word is the most important
-        subject = STOPWORD
+        subject = MarkovReqHandler.STOPWORD
         confidence = 0
 
         for word in words:
-            if wordcounts.has_key(word):
-                tfidf = tf_idf(word, words, wordcounts, sentences_ever)
+            if MarkovReqHandler.wordcounts.has_key(word):
+                tfidf = tf_idf(word, words, MarkovReqHandler.wordcounts, sentences_ever)
                 if tfidf > confidence:
                     confidence = tfidf
                     subject = word
 
         # pick a word pair we've seen used with that word before as a seed
         pairs = []
-        for wordpair in paircounts:
+        for wordpair in MarkovReqHandler.paircounts:
             temp = wordpair.split()
             if (temp[0] == subject) or ((len(temp) > 1) and (temp[1] == subject)):
-                pairs.append((wordpair, paircounts.get(wordpair)))
+                pairs.append((wordpair, MarkovReqHandler.paircounts.get(wordpair)))
 
         seed = choose_word_from_list(pairs)
 
@@ -202,32 +202,32 @@ class MarkovReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
  
         # forwards
         wordpair = seed
-        if dictionary.has_key(wordpair):
+        if MarkovReqHandler.dictionary.has_key(wordpair):
             chain = wordpair
         #print wordpair
-        while (wordpair.split()[1] != STOPWORD) and (dictionary.has_key(wordpair)):
+        while (wordpair.split()[1] != MarkovReqHandler.STOPWORD) and (MarkovReqHandler.dictionary.has_key(wordpair)):
             wordpair = wordpair.split()[1] + u' ' + \
-                        choose_word_from_list(dictionary.get(wordpair)[1])
+                        choose_word_from_list(MarkovReqHandler.dictionary.get(wordpair)[1])
             #print wordpair
             chain = chain + u' ' + wordpair.split()[1]
         # backwards
         wordpair = seed
-        if dictionary.has_key(wordpair) and wordpair.split()[0] != STOPWORD:
+        if MarkovReqHandler.dictionary.has_key(wordpair) and wordpair.split()[0] != MarkovReqHandler.STOPWORD:
             wordpair = choose_word_from_list(
-                dictionary.get(wordpair)[0]) + \
+                MarkovReqHandler.dictionary.get(wordpair)[0]) + \
                 u' ' + wordpair.split()[0]
         # so we don't have the seed twice
 
-        while (wordpair.split()[0] != STOPWORD) and (dictionary.has_key(wordpair)):
+        while (wordpair.split()[0] != MarkovReqHandler.STOPWORD) and (MarkovReqHandler.dictionary.has_key(wordpair)):
             #print wordpair
             chain = wordpair.split()[0] + u' ' + chain
             wordpair = choose_word_from_list(
-                dictionary.get(wordpair)[0]) + \
+                MarkovReqHandler.dictionary.get(wordpair)[0]) + \
                 u' ' + wordpair.split()[0]
 
         MarkovReqHandler.dictLock.release()
 
-        return chain.replace(STOPWORD, u'')
+        return chain.replace(MarkovReqHandler.STOPWORD, u'')
 
 
 def word_index_in_list(findword, word_list):
